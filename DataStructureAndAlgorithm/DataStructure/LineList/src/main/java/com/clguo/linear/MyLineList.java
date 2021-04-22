00b063e50fd9fa0238995aab19e74f3ef303b13f
clguo <475510328@qq.com>
2021-04-23 00:18:23 +0800

    add：linear list create

---
+++ DataStructureAndAlgorithm/DataStructure/LineList/src/main/java/com/clguo/linear/MyLineList.java
@@ -0,0 +1,17 @@
+package com.clguo.linear;
+
+public interface MyLineList<T> {
+
+    /**
+     * 线性表的长度
+     * @return
+     */
+    int length();
+
+    /**
+     * 获取i位置的元素
+     * @param i
+     * @return
+     */
+    T get(int i);
+}
